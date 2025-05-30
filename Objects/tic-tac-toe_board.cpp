57ffd3197fe45ca0031e06170f64e5b19ec471f5
clomollo <oyoocliff471@gmail.com>
2025-05-30 19:02:17 +0300

    Added useful comments for learning

--- Objects/tic-tac-toe_board.cpp
+++ Objects/tic-tac-toe_board.cpp
@@ -38,3 +38,27 @@ int main()
 
     cout << "\nThe game is over.\n 'X' wins!\n";
 }
+/*
+It’s possible to simply declare a multidimensional array without initializing it. Here’s an example:
+char chessBoard[8][8];
+The preceding code declares an 8*8, two-dimensional character array, chessBoard. By the
+way, multidimensional arrays aren’t required to have the same size for each dimension. The
+following is a perfectly valid declaration for a game map represented by individual characters:
+char map[12][20];
+*/
+
+//Indexing Multidimensional Arrays
+/*
+The next thing I do in the program is display the tic-tac-toe board. But before I
+get into the details of that, I want to explain how to index an individual array
+element. You index an individual element of a multidimensional array by
+supplying a value for each dimension of the array. That’s what I do to place
+an X in the array where a space was:
+board[1][0] = ’X’;
+The previous code assigns the character to the element at board[1][0] (which
+was ’ ’). Then I display the tic-tac-toe board after the move the same way I
+displayed it before the move.
+
+By using a pair of nested for loops, I move through the two-dimensional array
+and display the character elements as I go, forming a tic-tac-toe board.
+*/
